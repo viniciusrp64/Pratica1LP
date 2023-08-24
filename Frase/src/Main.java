99dabf6c6f7737257b82fef4a169f8c5f2e2990d
viniciusrp64 <62041895+viniciusrp64@users.noreply.github.com>
2023-08-24 19:03:44 -0300

    Add files via upload

---
+++ Frase/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite uma frase: ");
+        String frase = scanner.nextLine();
+
+        int numVogais = 0;
+        for (int i = 0; i < frase.length(); i++) {
+            char c = frase.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                    c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+                numVogais++;
+            }
+        }
+
+        System.out.println("Número de vogais: " + numVogais);
+    }
+}
